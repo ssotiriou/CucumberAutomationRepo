a40cbac898c4587d26ff239f608943bf87ae03ec
User <User@User-PC.Home>
2018-06-26 21:40:53 +0100

    benchmark

--- CucumberAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ CucumberAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -1,28 +1,40 @@
 package com.basic.FirstFeatureFile;
 
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.remote.DesiredCapabilities;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MyFirstStepDef {
 	
 	private WebDriver driver;
-	
+	//String nodeURL = "http://localhost:4444/wd/hub";
 	
 	@Given("^That the User is on the Facebook login page$")
-	public void the_User_is_on_the_Facebook_login_page() {
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\selenium 3 installs\\chromedriver_win32\\chromedriver.exe");
-		//driver = new ChromeDriver();
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\User\\Desktop\\selenium 3 installs\\geckodriver-v0.19.1-win64\\geckodriver.exe");
-    	driver = new FirefoxDriver();
+	public void the_User_is_on_the_Facebook_login_page() throws MalformedURLException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\selenium 3 installs\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		//Set the Remote Web Driver
+		//System.out.println("Firefox with MyFirstStepDef - started");
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\User\\Desktop\\selenium 3 installs\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+    	//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		//capabilities.setPlatform(Platform.WINDOWS);
+		//capabilities.setVersion("");
+		//driver = new RemoteWebDriver(new URL(nodeURL),capabilities);
+		
+		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
